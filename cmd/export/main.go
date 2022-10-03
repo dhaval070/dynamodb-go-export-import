21dc7429d3c5db9379f3b0f74886ee77b1c377d9
dhaval dhanani <dhaval070@gmail.com>
2022-10-04 01:13:25 +0530

    export output dynamodb format

--- cmd/export/main.go
+++ cmd/export/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	ddbtypes "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 func CreateLocalClient() *dynamodb.Client {
@@ -25,8 +26,35 @@ func CreateLocalClient() *dynamodb.Client {
 	return dynamodb.NewFromConfig(cfg)
 }
 
+func convertAttrValue(value ddbtypes.AttributeValue) (rec map[string]any) {
+	rec = map[string]any{}
+	// TODO: support all remaining types
+	switch t := value.(type) {
+	case *ddbtypes.AttributeValueMemberBOOL:
+		rec["BOOL"] = t.Value
+	case *ddbtypes.AttributeValueMemberL:
+		var values = []map[string]any{}
+
+		for _, v := range t.Value {
+			values = append(values, convertAttrValue(v))
+		}
+		rec["L"] = values
+	case *ddbtypes.AttributeValueMemberM:
+		var r = map[string]any{}
+		for k, v := range t.Value {
+			r[k] = convertAttrValue(v)
+		}
+		rec["M"] = r
+	case *ddbtypes.AttributeValueMemberN:
+		rec["N"] = t.Value
+	case *ddbtypes.AttributeValueMemberS:
+		rec["S"] = t.Value
+	}
+
+	return rec
+}
+
 func main() {
-	fmt.Println("vim-go")
 	client := CreateLocalClient()
 
 	table := "convrsUsers"
@@ -40,24 +68,23 @@ func main() {
 		panic(err)
 	}
 
-	var items = make([]map[string]any, 0, 1)
+	result := []map[string]any{}
 
-	for _, v := range output.Items {
-		js, err := json.Marshal(v)
-		if err != nil {
-			panic(err)
-		}
-		var obj map[string]any
-		err = json.Unmarshal(js, &obj)
-
-		if err != nil {
-			panic(err)
-		}
-
-		items = append(items, obj)
-		fmt.Println(string(js))
+	for _, item := range output.Items {
 		fmt.Println("---------------------")
+		var v = map[string]any{}
+
+		for attr, value := range item {
+			v[attr] = convertAttrValue(value)
+		}
+
+		result = append(result, v)
 	}
-	str, err := json.Marshal(items)
-	fmt.Println(string(str))
+
+	js, err := json.Marshal(result)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(js))
 }
