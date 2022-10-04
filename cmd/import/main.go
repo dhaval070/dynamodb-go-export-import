e09c3cfc5983f8c47ee726e20c7716b520bdc6f0
dhaval dhanani <dhaval070@gmail.com>
2022-10-04 23:34:38 +0530

    import item continue...

--- cmd/import/main.go
+++ cmd/import/main.go
@@ -12,6 +12,7 @@ import (
 
 	ddbclient "ddb-export/pkg/client"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	ddbtypes "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
@@ -43,11 +44,10 @@ func createTable(client *dynamodb.Client) {
 		},
 	}
 
-	output, err := client.CreateTable(context.Background(), &opt)
+	_, err := client.CreateTable(context.Background(), &opt)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(output)
 }
 
 func importCsv(client *dynamodb.Client, fd io.Reader) {
@@ -90,12 +90,79 @@ func importCsv(client *dynamodb.Client, fd io.Reader) {
 
 }
 
-func main() {
-	dir, _ := os.Getwd()
-	fmt.Println(dir)
+func convertAttrValue(item map[string]any) (attr ddbtypes.AttributeValue) {
+	for k, v := range item {
+		switch k {
+		case "BOOL":
+			return &ddbtypes.AttributeValueMemberBOOL{Value: v.(bool)}
+		case "S":
+			return &ddbtypes.AttributeValueMemberS{Value: v.(string)}
+		case "M":
+			var res = map[string]ddbtypes.AttributeValue{}
 
+			for h, v := range v.(map[string]any) {
+				res[h] = convertAttrValue(v.(map[string]any))
+			}
+			return &ddbtypes.AttributeValueMemberM{Value: res}
+		case "N":
+			return &ddbtypes.AttributeValueMemberN{Value: v.(string)}
+		case "L":
+			var vars = []ddbtypes.AttributeValue{}
+			for _, vv := range v.([]map[string]any) {
+				vars = append(vars, convertAttrValue(vv))
+			}
+			return &ddbtypes.AttributeValueMemberL{Value: vars}
+		default:
+			panic("unsupported type")
+		}
+	}
+	return attr
+}
+
+func convertItem(item map[string]any) map[string]ddbtypes.AttributeValue {
+	var ddbitem = map[string]ddbtypes.AttributeValue{}
+
+	for k, v := range item {
+		ddbitem[k] = convertAttrValue(v.(map[string]any))
+	}
+	return ddbitem
+}
+
+func importJson(client *dynamodb.Client, fd io.Reader) {
+	data, err := io.ReadAll(fd)
+	if err != nil {
+		panic(err)
+	}
+	var items = []map[string]any{}
+
+	err = json.Unmarshal(data, &items)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range items {
+		var ddbitem = convertItem(item)
+		fmt.Println(ddbitem)
+
+		var opt = &dynamodb.PutItemInput{
+			TableName: aws.String(tableName),
+			Item:      ddbitem,
+		}
+
+		client.PutItem(context.Background(), opt)
+		break
+	}
+}
+
+func main() {
 	bStaging := flag.Bool("staging", false, "use staging remote db")
+	sFilename := flag.String("file", "staging.json", "data file to import")
+
 	flag.Parse()
+	if *sFilename == "" {
+		panic("file is required")
+	}
+
 	var client *dynamodb.Client
 
 	log.Println(*bStaging)
@@ -108,10 +175,19 @@ func main() {
 
 	createTable(client)
 
-	fd, err := os.Open("results.csv")
+	var filename = *sFilename
+
+	fd, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 
-	importCsv(client, fd)
+	switch filename[len(filename)-4:] {
+	case "json":
+		importJson(client, fd)
+	case ".csv":
+		importCsv(client, fd)
+	default:
+		panic("unsupported input file format")
+	}
 }
